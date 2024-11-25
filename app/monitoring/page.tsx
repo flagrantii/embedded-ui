c2405947b40fa944916a0281325d2a7c74a44a4b
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-11-25 19:13:43 +0700

    fetch sensor data on monitoring page

--- app/monitoring/page.tsx
+++ app/monitoring/page.tsx
@@ -1,11 +1,51 @@
-import { Suspense } from 'react'
+'use client'
+import { Suspense, useEffect, useState } from 'react'
 import ParkingOverview from '@/components/parking/ParkingOverview'
 import CCTVFeed from '@/components/parking/CCTVFeed'
 import ParkingStats from '@/components/parking/ParkingStats'
 import ParkingHistory from '@/components/parking/ParkingHistory'
 import { Skeleton } from '@/components/ui/skeleton'
+import { database } from '../firebaseConfig'
+import { getDatabase, ref, get, onValue} from 'firebase/database'
+import { collection,getDocs, QuerySnapshot } from 'firebase/firestore'
+import { da } from 'date-fns/locale'
 
 export default function MonitoringPage() {
+  // async function fetch(){
+  //   const querySnapshot = await getDocs(collection(database,"ultrasonic/sensor1"))
+  //   const data = [];
+  //     querySnapshot.forEach((doc) => {
+  //     console.log(doc)
+  //     data.push({id:doc.id , ...doc.data()})
+  //   })
+  // }
+  useEffect(() => {
+    const dataRef = ref(database, 'ultrasonic')
+    // get(dataRef).then((snapshot) => {
+    //   if(snapshot.exists()){
+    //     console.log(snapshot.val())
+    //   }
+    // })
+    const unsubscribe = onValue(
+      dataRef,
+      (snapshot) => {
+        if(snapshot.exists()){
+          const data = snapshot.val();
+          const sensorValues = Object.values(data)
+          console.log('sensor1',sensorValues[0])
+          console.log('sensor2',sensorValues[1])
+          console.log('sensor3',sensorValues[2])
+
+        }else{
+          console.log('No data')
+        }
+      },
+      (error) => {
+        console.error('Error Fetching Data', error)
+      }
+    )
+  })
+  //const data = database
   return (
     <div className="container mx-auto p-6 space-y-6">
       <h1 className="text-3xl font-bold tracking-tight">Parking Monitoring System</h1>
